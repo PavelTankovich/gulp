a1674b6412a2259ae258606e757a0a29bac398ee
Tankovich Pavel <gavrikboss@gmail.com>
2019-08-22 00:38:16 +0300

    added task reload page and added default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,43 @@
-/** Подключение зависимостей */
+// Подключение зависимостей
 const pathSass = 'app/sass/**/*.scss';
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
 
-/** Создание тасков
-* gulp.task('mytask', () => {});
-*/
+// Создание тасков
+// gulp.task('mytask', () => {});
+
+/**
+ * таск преобразования sass => css
+ */
 gulp.task('sass', () => {
     return gulp.src(pathSass)
             .pipe(sass())
-            .pipe(gulp.dest('app/css'));
+            .pipe(gulp.dest('app/css'))
+            .pipe(browserSync.reload( {stream: true} )); // обновление после изменений
 });
 
+/**
+ * таск слежки за файлами
+ */
 gulp.task('watch', () => {
     gulp.watch(pathSass, gulp.parallel('sass'));
-})
+});
+
+/**
+ * таск перезагрузки страницы после изменений
+ */
+gulp.task('browser-sync', () => {
+    browserSync({
+        server : {
+            baseDir : 'app',
+        },
+        notify : false,
+    });
+});
+
+/**
+ * таск запуска очереди выполнения тасков
+ */
+gulp.task('default', gulp.parallel('browser-sync', 'sass', 'watch'));
