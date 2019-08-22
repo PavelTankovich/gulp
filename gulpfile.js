85e3069b3a12c010c9f3313464a96ad1c4a28175
Pavel Tankovich <p.tankovich@andersenlab.com>
2019-08-22 15:27:49 +0300

    add task code and scrits. added plagins js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,18 @@
 // Подключение зависимостей
 const pathSass = 'app/sass/**/*.scss';
+const pathHTML = 'app/*.html';
+const pathScripts = [
+    'app/libs/jquery/dist/jquery.min.js',
+    'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+];
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglifyjs');
 
 // Создание тасков
-// gulp.task('mytask', () => {});
-
-/**
- * таск преобразования sass => css
- */
-gulp.task('sass', () => {
-    return gulp.src(pathSass)
-            .pipe(sass())
-            .pipe(gulp.dest('app/css'))
-            .pipe(browserSync.reload( {stream: true} )); // обновление после изменений
-});
-
-/**
- * таск слежки за файлами
- */
-gulp.task('watch', () => {
-    gulp.watch(pathSass, gulp.parallel('sass'));
-});
 
 /**
  * таск перезагрузки страницы после изменений
@@ -38,6 +27,44 @@ gulp.task('browser-sync', () => {
 });
 
 /**
+ * таск преобразования sass => css
+ */
+gulp.task('code', () => {
+    return gulp.src(pathHTML)
+            .pipe(browserSync( {stream: true} ));
+});
+
+/**
+ * таск преобразования sass => css
+ */
+gulp.task('sass', () => {
+    return gulp.src(pathSass)
+            .pipe(sass())
+            .pipe(gulp.dest('app/css'))
+            .pipe(browserSync.reload( {stream: true} )); // обновление после изменений
+});
+
+/**
+ * таск преобразования объединения и минификации js файлов
+ */
+gulp.task('scripts', () => {
+    return gulp.src(pathScripts)
+            .pipe(concat('libs.min.js'))
+            .pipe(uglify()) // минификация
+            .pipe(gulp.dest('app/js'))
+            .pipe(browserSync.reload( {stream: true} ));
+});
+
+/**
+ * таск слежки за файлами
+ */
+gulp.task('watch', () => {
+    gulp.watch(pathSass, gulp.parallel('sass'));
+    gulp.wath(pathHTML, gulp.parallel('code'));
+    gulp.watch(pathScripts, gulp.parallel('scripts'));
+});
+
+/**
  * таск запуска очереди выполнения тасков
  */
-gulp.task('default', gulp.parallel('browser-sync', 'sass', 'watch'));
+gulp.task('default', gulp.parallel('browser-sync', 'sass', 'scripts', 'watch'));
